34ab504cf3201eba661773b5aa915ad9ce70b209
Eren <vrsozluk@gmail.com>
2022-10-30 21:18:21 +0300

    Click to draw function has been added

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
+let color = 'black';
+let click = false;
 
 document.addEventListener('DOMContentLoaded',function(){
-        createBoard(2);
-        console.log('hi')
+        createBoard(16);
+
+        document.querySelector('body').addEventListener('click', function(e){
+                if(e.target.tagName != 'BUTTON'){
+                        click = !click;
+                        let draw = document.querySelector('#draw');
+                        if(click){
+                                draw.innerHTML = 'now you can draw!'
+                        }else{
+                                draw.innerHTML = "You're not allowed to draw!"
+                        }
+                }
+        })
+
+        let btn_popup = document.querySelector('#popup');
+        btn_popup.addEventListener('click', function(){
+                let size = getSize();
+                createBoard(size);
+        })
 })
 
 function createBoard(size){
@@ -14,7 +33,43 @@ function createBoard(size){
         for(let i = 0; i < numDivs; i++){
                 let div = document.createElement('div');
                 board.insertAdjacentElement('beforeend',div);
-                div.style.backgroundColor = `purple`;
-                div.style.border = `1px solid black`;
+                div.style.backgroundColor = `white`;
+                div.addEventListener('mouseover', colorDiv)
+                
         }
 }
+
+function getSize(){
+        let input = prompt('Enter a Size');
+        let message = document.querySelector('#message');
+        if(input == ''){
+                message.innerHTML = `please provide a number`;
+        }else if(input < 1 || input > 100){
+                message.innerHTML = 'Provide a number between 1 and 100';
+        }else {
+                message.innerHTML = `u can play now`
+                return input;
+        }
+
+}
+
+function colorDiv(){
+      if(click){
+        if(color == 'random'){
+                this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+        }else{
+                this.style.backgroundColor = 'black';
+        }
+}
+
+
+}
+function setColor(colorChoice){
+         color = colorChoice
+}
+
+function resetBoard(){
+        let divs = document.querySelectorAll('div');
+        divs.forEach((div) => div.style.backgroundColor = 'white');
+}
+
