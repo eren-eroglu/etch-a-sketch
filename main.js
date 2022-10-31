7a1df5e62bf1eaa708086d3d18973a86aa8e3904
Eren <vrsozluk@gmail.com>
2022-10-31 12:52:25 +0300

    Eraser button has been added

--- main.js
+++ main.js
@@ -1,75 +1,77 @@
 let color = 'black';
 let click = false;
 
-document.addEventListener('DOMContentLoaded',function(){
-        createBoard(16);
+document.addEventListener('DOMContentLoaded', function(){
+       createBoard(32);
 
-        document.querySelector('body').addEventListener('click', function(e){
-                if(e.target.tagName != 'BUTTON'){
-                        click = !click;
-                        let draw = document.querySelector('#draw');
-                        if(click){
-                                draw.innerHTML = 'now you can draw!'
-                        }else{
-                                draw.innerHTML = "You're not allowed to draw!"
-                        }
+       document.querySelector('body').addEventListener('click', function(e){
+        if(e.target.tagName != 'BUTTON'){
+                click = !click;
+                let draw = document.querySelector('#draw');
+                if(click){
+                        draw.innerHTML = 'Now you can draw!';
+                }else{
+                        draw.innerHTML = "You're not allowed to draw!"
                 }
-        })
+        }
 
-        let btn_popup = document.querySelector('#popup');
-        btn_popup.addEventListener('click', function(){
+       })
+
+       let btn_popup = document.querySelector('#popup');
+       btn_popup.addEventListener('click', function(){
                 let size = getSize();
                 createBoard(size);
-        })
+       })
 })
 
 function createBoard(size){
-        let board = document.querySelector('.board');
-        board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-        board.style.gridTemplateRows    = `repeat(${size}, 1fr)`;
+        const board = document.querySelector('.board');
+       board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+       board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-        let numDivs = size * size;
+       let numDivs = size * size;
 
-        for(let i = 0; i < numDivs; i++){
-                let div = document.createElement('div');
-                board.insertAdjacentElement('beforeend',div);
-                div.style.backgroundColor = `white`;
-                div.addEventListener('mouseover', colorDiv)
-                
-        }
+       for(let i = 0; i < numDivs; i++){
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', colorDiv)
+        board.insertAdjacentElement('beforeend', div);
+       }
 }
 
-function getSize(){
-        let input = prompt('Enter a Size');
-        let message = document.querySelector('#message');
-        if(input == ''){
-                message.innerHTML = `please provide a number`;
-        }else if(input < 1 || input > 100){
-                message.innerHTML = 'Provide a number between 1 and 100';
-        }else {
-                message.innerHTML = `u can play now`
+        function getSize(){
+                let input = prompt('Enter a Size');
+                let message = document.querySelector('#message');
+                if(input == ''){message.innerHTML = 'Please type a number'
+        }else if(input < 0 || input > 100){
+                message.innerHTML = 'Type a number between 1 and 100!'
+        }else{
+                message.innerHTML = 'Now you can play!'
                 return input;
         }
-
-}
-
-function colorDiv(){
-      if(click){
-        if(color == 'random'){
-                this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
-        }else if(color == 'black'){
-                this.style.backgroundColor = 'black';
         }
-}
+
+        function colorDiv(){
+                if(click){
+                if(color == 'random'){
+                        this.style.backgroundColor = `hsl(${Math.random() * 360 },100%, 50%)`;
+                       
+                }else if(color == 'black'){
+                        this.style.backgroundColor = 'black';
+                        
+                }else if(color == 'white'){
+                        this.style.backgroundColor = 'white';
+                        
+                }
+        }
+        }
+
+        function setColor(colorChoice){
+                 color = colorChoice;
+        }
 
 
-}
-function setColor(colorChoice){
-         color = colorChoice
-}
-
-function resetBoard(){
+       function resetBoard(){
         let divs = document.querySelectorAll('div');
-        divs.forEach((div) => div.style.backgroundColor = 'white');
-}
-
+        divs.forEach((div) => div.style.backgroundColor = 'white')
+       }
+     
